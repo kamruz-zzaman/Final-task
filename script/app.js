ac9dce6a2935e2c39a9a6d549dc2dd145cd443db
Md Nazmul Islam <kamruz.zaman8991@gmail.com>
2021-12-22 12:37:45 +0600

    Desktop Scrolling with SVG Done

--- script/app.js
+++ script/app.js
@@ -1,5 +1,16 @@
+const path = document.getElementById('Opaque_Ring')
+const pathLength = path.getTotalLength();
 
+path.style.strokeDasharray = pathLength;
+path.style.strokeDashoffset = pathLength;
 
-document.addEventListener('scroll', function (e) {
-    console.log(window.innerHeight / 100)
-});
+const drawWhenScroll = () => {
+    const drawPath = pathLength - (pathLength * calcScrollPercent());
+    console.log(drawPath);
+    path.style.strokeDashoffset = drawPath;
+}
+const calcScrollPercent = () =>{
+    let height = window.document.getElementById('cont').scrollHeight - window.document.getElementById('cont').clientHeight;
+    return window.document.getElementById('cont').scrollTop / height;
+}
+window.document.getElementById('cont').addEventListener('scroll',drawWhenScroll)
