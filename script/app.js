22e18049918a06ce6a37342b8b0b7dd8fc732e4a
kamruz-zaman8991 <70217992+kamruz-zaman8991@users.noreply.github.com>
2021-12-22 17:38:43 +0600

    mobile responsive done

--- script/app.js
+++ script/app.js
@@ -9,8 +9,16 @@ const drawWhenScroll = () => {
     console.log(drawPath);
     path.style.strokeDashoffset = drawPath;
 }
-const calcScrollPercent = () =>{
+const calcScrollPercent = () => {
     let height = window.document.getElementById('cont').scrollHeight - window.document.getElementById('cont').clientHeight;
     return window.document.getElementById('cont').scrollTop / height;
 }
-window.document.getElementById('cont').addEventListener('scroll',drawWhenScroll)
+window.document.getElementById('cont').addEventListener('scroll', drawWhenScroll)
+
+$('.autoplay').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true
+});
